908cce6a6b86019994afbdb091a053e57504d1ea
tahini-dev <tahini.dev@gmail.com>
2020-10-12 17:35:38 +0100

    remove classifiers for now

--- setup.py
+++ setup.py
@@ -15,11 +15,5 @@ setup(
     install_requires=[
     ],
     python_requires='>=3.7',
-    classifiers=[
-        'Programming Language:: Python :: 3',
-        'Programming Language:: Python :: 3.7',
-        'Programming Language:: Python :: 3.8',
-        'Operating System :: OS Independent',
-    ],
     include_package_data=True
 )
