9b5a9d0ea8051dbbb96aca5692b0eaabc75e8be1
tahini-dev <tahini.dev@gmail.com>
2020-10-12 17:44:37 +0100

    remove long_description in setup.py for now

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup, find_packages
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
 setup(
     name='tahini',
     version='2020.10.1',
     author='tahini-dev',
     author_email='tahini.dev@gmail.com',
     description='Python package for graph theory',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     url='https://github.com/tahini-dev/tahini',
     packages=find_packages(exclude=('tests',)),
     install_requires=[
