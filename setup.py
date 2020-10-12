1f7fe0148fa59f03d152352eb6e7f755c5ddd16c
tahini-dev <tahini.dev@gmail.com>
2020-10-12 17:48:04 +0100

    add long_description in setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author='tahini-dev',
     author_email='tahini.dev@gmail.com',
     description='Python package for graph theory',
+    long_description='https://github.com/tahini-dev/tahini',
     url='https://github.com/tahini-dev/tahini',
     packages=find_packages(exclude=('tests',)),
     install_requires=[
