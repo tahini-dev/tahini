a0f28c55f7a401bc739c8c3cbeb4d78049a85bcb
tahini-dev <tahini.dev@gmail.com>
2020-10-13 09:54:22 +0100

    add id property for node and change tests

--- tahini/node.py
+++ tahini/node.py
@@ -1,6 +1,11 @@
-from uuid import uuid4
+from __future__ import annotations
+from uuid import uuid4, UUID
 
 
 class Node:
     def __init__(self):
         self._id = uuid4()
+
+    @property
+    def id(self) -> UUID:
+        return self._id
--- tests/test_node.py
+++ tests/test_node.py
@@ -1,6 +1,16 @@
+import pytest
+
 import tahini.node
 
 
-def test_node():
+def test_node_init():
     node = tahini.node.Node()
-    assert hasattr(node, '_id')
+    assert isinstance(node, tahini.node.Node)
+
+
+@pytest.mark.parametrize('node, attribute', [
+    (tahini.node.Node(), '_id'),
+    (tahini.node.Node(), 'id'),
+])
+def test_node_properties(node, attribute):
+    assert hasattr(node, attribute)
