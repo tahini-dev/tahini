6e5a1414b9803b8c4d215277bcf90b72ce62bfcf
tahini-dev <tahini.dev@gmail.com>
2021-01-12 23:32:04 +0000

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='tahini',
-    version='2020.10.2',
+    version='2021.01.1',
     author='tahini-dev',
     author_email='tahini.dev@gmail.com',
     description='Python package for graph theory',
