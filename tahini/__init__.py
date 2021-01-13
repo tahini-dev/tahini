babc7fa28953d99ed160d6a541b52bdce378cb6d
tahini-dev <tahini.dev@gmail.com>
2021-01-13 13:59:40 +0000

    add to init

--- tahini/__init__.py
+++ tahini/__init__.py
@@ -2,3 +2,11 @@ from pkg_resources import get_distribution as _get_distribution
 
 __author__ = 'tahini-dev'
 __version__ = _get_distribution('tahini').version
+
+from . import (
+    core,
+)
+
+from .core import (
+    Graph,
+)
