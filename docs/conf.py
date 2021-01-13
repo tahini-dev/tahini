bade97bc4e8c1b14087922f7c908664daf9ae0b3
tahini-dev <tahini.dev@gmail.com>
2021-01-13 13:19:11 +0000

    logging path

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+# -- Autodoc command ---------------------------------------------------------
+# sphinx-apidoc -f -o docs/ tahini/
+# ----------------------------------------------------------------------------
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
