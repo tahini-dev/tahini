510735c28260a4f8cb001374cf7647e731eb28e3
tahini-dev <tahini.dev@gmail.com>
2021-02-11 11:01:07 +0000

    try adding check_names=True

--- tahini/testing.py
+++ tahini/testing.py
@@ -43,6 +43,7 @@ def assert_container_equal(
         check_index_type=check_index_type,
         check_column_type=check_column_type,
         check_like=check_like,
+        check_names=True,
         obj=obj,
         **kwargs,
     )
